eb7c6f700677c9576968a2b29dbe0504428ec34c
Michael McNees <mcnees.michael@gmail.com>
2021-05-09 20:48:16 -0400

    Removed unneccesary code from examples during lecture

--- web/app/index.js
+++ web/app/index.js
@@ -8,28 +8,6 @@ const error = require('debug')('web:error')
 const app = express()
 // set up a folder to hold all the static files
 app.use(express.static('public'))
-// example middleware that runs once for every request
-app.use((req, res, next) => {
-    log('\nRuns once for every request')
-    setTimeout(() => { next(); }, 2000)
-}, (req, res, next) => {
-    log('Will run when next step is called')
-    next()
-})
-
-// route specific middleware
-app.use('/about', (req, res, next) => {
-    log('Runs only on the /about page')
-    next(new Error('Not authorized'))
-})
-
-// four params are required to mark this as an error handling middleware
-// the commend below allows for eslint to not throw an error because I am not using the enxt function
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-    error('Error found:', err)
-    res.sendStatus(500)
-})
 
 // export the express app
 module.exports = app
--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@
 const log = require('debug')('web:logging')
 // get the express application
 const app = require('./app')
-// set the port to either the one passed from the encironment variables or 3000
+// set the port to either the one passed from the environment variables or 3000
 const port = process.nextTick.PORT || 3000
 // spin up the server and log what port it is running on
 app.listen(port, () => log(`Web listening on port ${port}!`))
